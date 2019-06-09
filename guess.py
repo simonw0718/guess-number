46225be4a1608e1505dab2826ee32d09c86724ac
Simon Wang <simonw0718@gmail.com>
2019-06-09 15:57:04 +0800

    'v2'

--- guess.py
+++ guess.py
@@ -1,17 +1,26 @@
+g_range_min = input('請輸入下限:')
+g_range_max = input('請輸入上限:')
+inputword = '請輸入數字介於'+ g_range_min+ '至' + g_range_max+ '之間:'
+g_range_min = int(g_range_min)
+g_range_max = int(g_range_max)
+
 import random
-answer = random.randint(1,10)
-test = input('請輸入1~10數字:')
+answer = random.randint(g_range_min,g_range_max)
+test = input(inputword)
 test = int(test)
-while test != answer:
+time = 1
+while test != answer:	
     if test > answer:
         print('比答案大!')
-        test = input('請輸入1~10數字:')
+        test = input(inputword)
         test = int(test)
+        time = time + 1
     elif test < answer:
         print('比答案小!')
-        test = input('請輸入1~10數字:')
+        test = input(inputword)
         test = int(test)
-print('終於猜對了!答案是:',answer)
+        time = time + 1
+print('終於猜對了!', '你共猜了', time, '次', '答案是:',answer)
 
 
 
